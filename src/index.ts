c10456b4e9481ff3f4add283bbdabea4888cc6cb
Yannick <yannick.franssen@outlook.com>
2019-11-12 16:59:39 +0100

    Drop excessive browsers

--- src/index.ts
+++ src/index.ts
@@ -13,154 +13,49 @@ export class BrowserInfo
   ) {}
 }
 
-export class NodeInfo implements DetectedInfo<'node', NodeJS.Platform, string> {
-  public readonly name: 'node' = 'node';
-  public readonly os: NodeJS.Platform = process.platform;
-
-  constructor(public readonly version: string) {}
-}
-
-export class BotInfo implements DetectedInfo<'bot', null, null> {
-  public readonly bot: true = true; // NOTE: deprecated test name instead
-  public readonly name: 'bot' = 'bot';
-  public readonly version: null = null;
-  public readonly os: null = null;
-}
-
 export type Browser =
-  | 'aol'
   | 'edge'
-  | 'edge-ios'
-  | 'yandexbrowser'
-  | 'vivaldi'
-  | 'kakaotalk'
-  | 'samsung'
-  | 'silk'
-  | 'miui'
-  | 'beaker'
-  | 'edge-chromium'
   | 'chrome'
-  | 'chromium-webview'
-  | 'phantomjs'
-  | 'crios'
+  | 'chrome'
   | 'firefox'
-  | 'fxios'
-  | 'opera-mini'
-  | 'opera'
-  | 'ie'
-  | 'bb10'
   | 'android'
-  | 'ios'
   | 'safari'
-  | 'facebook'
-  | 'instagram'
-  | 'ios-webview'
-  | 'searchbot';
+  | 'ios';
 export type OperatingSystem =
   | 'iOS'
   | 'Android OS'
-  | 'BlackBerry OS'
-  | 'Windows Mobile'
   | 'Amazon OS'
-  | 'Windows 3.11'
-  | 'Windows 95'
-  | 'Windows 98'
-  | 'Windows 2000'
-  | 'Windows XP'
-  | 'Windows Server 2003'
-  | 'Windows Vista'
-  | 'Windows 7'
-  | 'Windows 8'
-  | 'Windows 8.1'
-  | 'Windows 10'
-  | 'Windows ME'
-  | 'Open BSD'
-  | 'Sun OS'
+  | 'Windows'
   | 'Linux'
   | 'Mac OS'
-  | 'QNX'
-  | 'BeOS'
-  | 'OS/2'
-  | 'Chrome OS'
-  | 'Search Bot';
+  | 'Chrome OS';
 type UserAgentRule = [Browser, RegExp];
 type UserAgentMatch = [Browser, RegExpExecArray] | false;
 type OperatingSystemRule = [OperatingSystem, RegExp];
 
-// tslint:disable-next-line:max-line-length
-const SEARCHBOX_UA_REGEX = /alexa|bot|crawl(er|ing)|facebookexternalhit|feedburner|google web preview|nagios|postrank|pingdom|slurp|spider|yahoo!|yandex/;
-const SEARCHBOT_OS_REGEX = /(nuhk)|(Googlebot)|(Yammybot)|(Openbot)|(Slurp)|(MSNBot)|(Ask Jeeves\/Teoma)|(ia_archiver)/;
-const REQUIRED_VERSION_PARTS = 3;
-
 const userAgentRules: UserAgentRule[] = [
-  ['aol', /AOLShield\/([0-9\._]+)/],
+  ['edge', /Edg\/([0-9\._]+)/],
   ['edge', /Edge\/([0-9\._]+)/],
-  ['edge-ios', /EdgiOS\/([0-9\._]+)/],
-  ['yandexbrowser', /YaBrowser\/([0-9\._]+)/],
-  ['vivaldi', /Vivaldi\/([0-9\.]+)/],
-  ['kakaotalk', /KAKAOTALK\s([0-9\.]+)/],
-  ['samsung', /SamsungBrowser\/([0-9\.]+)/],
-  ['silk', /\bSilk\/([0-9._-]+)\b/],
-  ['miui', /MiuiBrowser\/([0-9\.]+)$/],
-  ['beaker', /BeakerBrowser\/([0-9\.]+)/],
-  ['edge-chromium', /Edg\/([0-9\.]+)/],
-  [
-    'chromium-webview',
-    /(?!Chrom.*OPR)wv\).*Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/,
-  ],
+  ['edge', /EdgA\/([0-9\._]+)/],
+  ['edge', /EdgiOS\/([0-9\._]+)/],
+  ['chrome', /CriOS\/([0-9\.]+)(:?\s|$)/],
   ['chrome', /(?!Chrom.*OPR)Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/],
-  ['phantomjs', /PhantomJS\/([0-9\.]+)(:?\s|$)/],
-  ['crios', /CriOS\/([0-9\.]+)(:?\s|$)/],
   ['firefox', /Firefox\/([0-9\.]+)(?:\s|$)/],
-  ['fxios', /FxiOS\/([0-9\.]+)/],
-  ['opera-mini', /Opera Mini.*Version\/([0-9\.]+)/],
-  ['opera', /Opera\/([0-9\.]+)(?:\s|$)/],
-  ['opera', /OPR\/([0-9\.]+)(:?\s|$)/],
-  ['ie', /Trident\/7\.0.*rv\:([0-9\.]+).*\).*Gecko$/],
-  ['ie', /MSIE\s([0-9\.]+);.*Trident\/[4-7].0/],
-  ['ie', /MSIE\s(7\.0)/],
-  ['bb10', /BB10;\sTouch.*Version\/([0-9\.]+)/],
-  ['android', /Android\s([0-9\.]+)/],
+  ['firefox', /FxiOS\/([0-9\.]+)/],
   ['ios', /Version\/([0-9\._]+).*Mobile.*Safari.*/],
   ['safari', /Version\/([0-9\._]+).*Safari/],
-  ['facebook', /FBAV\/([0-9\.]+)/],
-  ['instagram', /Instagram\s([0-9\.]+)/],
-  ['ios-webview', /AppleWebKit\/([0-9\.]+).*Mobile/],
-  ['ios-webview', /AppleWebKit\/([0-9\.]+).*Gecko\)$/],
-  ['searchbot', SEARCHBOX_UA_REGEX],
 ];
 const operatingSystemRules: OperatingSystemRule[] = [
   ['iOS', /iP(hone|od|ad)/],
   ['Android OS', /Android/],
-  ['BlackBerry OS', /BlackBerry|BB10/],
-  ['Windows Mobile', /IEMobile/],
   ['Amazon OS', /Kindle/],
-  ['Windows 3.11', /Win16/],
-  ['Windows 95', /(Windows 95)|(Win95)|(Windows_95)/],
-  ['Windows 98', /(Windows 98)|(Win98)/],
-  ['Windows 2000', /(Windows NT 5.0)|(Windows 2000)/],
-  ['Windows XP', /(Windows NT 5.1)|(Windows XP)/],
-  ['Windows Server 2003', /(Windows NT 5.2)/],
-  ['Windows Vista', /(Windows NT 6.0)/],
-  ['Windows 7', /(Windows NT 6.1)/],
-  ['Windows 8', /(Windows NT 6.2)/],
-  ['Windows 8.1', /(Windows NT 6.3)/],
-  ['Windows 10', /(Windows NT 10.0)/],
-  ['Windows ME', /Windows ME/],
-  ['Open BSD', /OpenBSD/],
-  ['Sun OS', /SunOS/],
+  ['Windows', /Windows/],
   ['Chrome OS', /CrOS/],
   ['Linux', /(Linux)|(X11)/],
   ['Mac OS', /(Mac_PowerPC)|(Macintosh)/],
-  ['QNX', /QNX/],
-  ['BeOS', /BeOS/],
-  ['OS/2', /OS\/2/],
-  ['Search Bot', SEARCHBOT_OS_REGEX],
 ];
 
-export function detect(
-  userAgent?: string,
-): BrowserInfo | BotInfo | NodeInfo | null {
+export function detect(userAgent?: string): BrowserInfo | null | boolean {
   if (!!userAgent) {
     return parseUserAgent(userAgent);
   }
@@ -169,10 +64,10 @@ export function detect(
     return parseUserAgent(navigator.userAgent);
   }
 
-  return getNodeVersion();
+  return false;
 }
 
-export function parseUserAgent(ua: string): BrowserInfo | BotInfo | null {
+export function parseUserAgent(ua: string): BrowserInfo | null {
   // opted for using reduce here rather than Array#first with a regex.test call
   // this is primarily because using the reduce we only perform the regex
   // execution once rather than once for the test and for the exec again below
@@ -196,15 +91,12 @@ export function parseUserAgent(ua: string): BrowserInfo | BotInfo | null {
   }
 
   const [name, match] = matchedRule;
-  if (name === 'searchbot') {
-    return new BotInfo();
-  }
-  let versionParts = match[1] && match[1].split(/[._]/).slice(0, 3);
+  let versionParts = match[1] && match[1].split(/[._]/).slice(0, 2);
   if (versionParts) {
-    if (versionParts.length < REQUIRED_VERSION_PARTS) {
+    if (versionParts.length < 2) {
       versionParts = [
         ...versionParts,
-        ...createVersionParts(REQUIRED_VERSION_PARTS - versionParts.length),
+        ...createVersionParts(2 - versionParts.length),
       ];
     }
   } else {
@@ -224,11 +116,6 @@ export function detectOS(ua: string): OperatingSystem | null {
   }
 
   return null;
-}
-
-export function getNodeVersion(): NodeInfo | null {
-  const isNode = typeof process !== 'undefined' && process.version;
-  return isNode ? new NodeInfo(process.version.slice(1)) : null;
 }
 
 function createVersionParts(count: number): string[] {
--- test/highLevel.js
+++ test/highLevel.js
@@ -1,26 +1,21 @@
-var semver = require('semver');
 var { detect } = require('../');
 var test = require('tape');
 
+const ua = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36';
+
 test('can run detection', function(t) {
-  t.ok(detect(), 'detection ran ok');
+  t.ok(detect(ua), 'detection ran ok');
   t.end();
 });
 
 test('name detected', function(t) {
-  const browser = detect();
+  const browser = detect(ua);
   t.ok(browser.name, browser.name);
   t.end();
 });
 
 test('version detected', function(t) {
-  const browser = detect();
+  const browser = detect(ua);
   t.ok(browser.version, browser.version);
-  t.end();
-});
-
-test('version is semver compatible', function(t) {
-  const browser = detect();
-  t.ok(semver.valid(browser.version));
   t.end();
 });
